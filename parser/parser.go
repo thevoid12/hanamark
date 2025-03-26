18fcdf9215811dab539a35daced46c3318796b8e
thevoid12 <thevoiddd1@gmail.com>
2025-03-26 22:41:56 +0530

    parse sub folder wip

--- parser/parser.go
+++ parser/parser.go
@@ -16,8 +16,6 @@ func CreateUpdateBasefile(ctx context.Context) error {
 
 	l := logs.GetLoggerctx(ctx)
 
-	rootSrcDir := viper.GetString("filepath.sourceMDRoot")
-	rootDestDir := viper.GetString("filepath.destMDRoot")
 	// baseFileMap has the list of base files which has subfolder in them so these basefiles
 	// should have links to the subfiles in it eg blogs.html should have links of all the blogs
 	// in its corresponding subfolder
@@ -33,41 +31,55 @@ func CreateUpdateBasefile(ctx context.Context) error {
 				return err
 			}
 		}
+		err = parseSubFolderFilesToHtml(ctx, bfdir)
+		if err != nil {
+			l.Sugar().Error("parse subfolder files to html failed", err)
+			return err
+		}
+	}
+	return nil
+}
 
-		// traverse through the sub directory of src  and create links to the base file in destination
-		err = filepath.Walk(filepath.Join(rootDestDir, bfdir), func(path string, info os.FileInfo, err error) error {
+func parseSubFolderFilesToHtml(ctx context.Context, baseFiledir string) error {
+	l := logs.GetLoggerctx(ctx)
+
+	rootSrcDir := viper.GetString("filepath.sourceMDRoot")
+	rootDestDir := viper.GetString("filepath.destMDRoot")
+
+	// traverse through the sub directory of src  and create links to the base file in destination
+	err := filepath.Walk(filepath.Join(rootDestDir, baseFiledir), func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Process only Markdown files
+		if !info.IsDir() && strings.HasSuffix(info.Name(), ".md") {
+			// Determine relative path from source root
+			relPath, err := filepath.Rel(rootSrcDir, path)
 			if err != nil {
 				return err
 			}
 
-			// Process only Markdown files
-			if !info.IsDir() && strings.HasSuffix(info.Name(), ".md") {
-				// Determine relative path from source root
-				relPath, err := filepath.Rel(rootSrcDir, path)
-				if err != nil {
-					return err
-				}
-
-				// Construct the corresponding destination path
-				destPath := filepath.Join(rootDestDir, relPath)
-				destDir := filepath.Dir(destPath)
-
-				// Ensure the destination directory exists
-				err = os.MkdirAll(destDir, os.ModePerm)
-				if err != nil {
-					l.Sugar().Error("make destination director failed", err)
-					return err
-				}
-
-				// Generate markdown with file links
-				err = ParseMarkdownToHtml(filepath.Join(rootSrcDir, bfdir), destPath)
-				if err != nil {
-					l.Sugar().Error("Error parsing markdown to html", err)
-					return err
-				}
+			// Construct the corresponding destination path
+			destPath := filepath.Join(rootDestDir, relPath)
+			destDir := filepath.Dir(destPath)
+			extension := filepath.Ext(destDir)
+			destDir = strings.TrimSuffix(destDir, extension) + ".html"
+			// Ensure the destination directory exists
+			err = os.MkdirAll(destDir, os.ModePerm)
+			if err != nil {
+				l.Sugar().Error("make destination director failed", err)
+				return err
 			}
-			return nil
-		})
-	}
-	return nil
+
+			// Generate markdown with file links
+			err = ParseMarkdownToHtml(filepath.Join(rootSrcDir, baseFiledir), destPath)
+			if err != nil {
+				l.Sugar().Error("Error parsing markdown to html", err)
+				return err
+			}
+		}
+		return nil
+	})
+	return err
 }
