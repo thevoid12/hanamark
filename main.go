bfd3c1f727ab9cc77d3e1c4b49a7a509d93b2085
thevoid12 <iamvinod2001@gmail.com>
2025-03-26 11:59:12 +0530

    viper setup and zap logger setup done

--- main.go
+++ main.go
@@ -1,8 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	logs "hanamark/logger"
+	"log"
+
+	"github.com/joho/godotenv"
+	"github.com/spf13/viper"
+)
 
 func main() {
 	fmt.Println("hiiii bitch this is hanamark")
+	err := godotenv.Load()
+	if err != nil {
+		log.Println("there is a error loading environment variables", err)
+		return
+	}
+	viper.SetConfigName("config")
+	viper.SetConfigType("json")
+	viper.AddConfigPath("./") // path to look for the config file in
+
+	err = viper.ReadInConfig()
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; ignore error if desired
+			log.Println("there is a error in the path of config file", err)
+		} else {
+			// Config file was found but another error was produced
+			log.Println("error laoding config file from viper", err)
+		}
+	}
+
+	l, err := logs.InitializeLogger()
+	if err != nil {
+		log.Println("error initializing logger", err)
+	}
+
+	ctx := context.Background()
+	ctx = logs.SetLoggerctx(ctx, l)
+
+	l.Sugar().Info("cache initialized successfully")
 
 }
