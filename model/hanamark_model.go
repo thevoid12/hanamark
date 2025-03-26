4b42f5fb0e2405ba3d70659f9f9daa968309e08e
thevoid12 <iamvinod2001@gmail.com>
2025-03-26 22:41:56 +0530

    parse sub folder wip

---
+++ model/hanamark_model.go
@@ -0,0 +1,13 @@
+package model
+
+import "time"
+
+// data for each page. this is a flat structure
+// so can be customized based on usecase
+type PageMeta struct {
+	GenHtml   string // generated html
+	PageName  string
+	PageTitle string
+	Date      time.Time
+	PageDir   string
+}
